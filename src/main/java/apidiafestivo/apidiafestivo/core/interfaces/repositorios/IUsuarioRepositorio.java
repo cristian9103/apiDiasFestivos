8236affce90c049d468a4dfcbc3ab6db4781ca02
Cristian <cristiankmilo1103@gmail.com>
2024-11-11 14:00:41 -0500

    Se finaliza el esquema de seguridad para springboot

--- src/main/java/apidiafestivo/apidiafestivo/core/interfaces/repositorios/IUsuarioRepositorio.java
+++ src/main/java/apidiafestivo/apidiafestivo/core/interfaces/repositorios/IUsuarioRepositorio.java
@@ -10,4 +10,7 @@ public interface IUsuarioRepositorio extends JpaRepository<Usuario, Integer> {
     @Query("SELECT u FROM Usuario u WHERE u.usuario = ?1")
     Usuario obtener(String Usuario);
 
+    @Query("SELECT u FROM Usuario u WHERE u.usuario = ?1 AND u.clave=?2")
+    Usuario validarUsuario(String usuario, String clave);
+
 }
